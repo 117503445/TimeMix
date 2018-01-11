a89505e278db07cb87f5496dc6825f4a1c030862
117503445 <t117503445@gmail.com>
2018-01-11 13:11:34 +0800

    demo0111_1311

--- TimeMix/ScheduleWindow.xaml.cs
+++ TimeMix/ScheduleWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using System.Xml.Linq;
 
 namespace TimeMix
@@ -25,18 +26,22 @@ namespace TimeMix
         {
             InitializeComponent();
             Controls = new Control[] { Lbl0, Lbl1, Lbl2 };
+            DispatcherTimer timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromHours(5)
+            };
+            timer.Tick += (s, e) => { Update(); };
+            timer.Start();
+            Load();
             Update();
         }
+        Schedules[] schedules;
         /// <summary>
-        /// 更新数据
+        /// 加载数据
         /// </summary>
-        public void Update()
+        public void Load()
         {
-            foreach (var item in Controls)
-            {
-                ((Label)item).Content = "";
-                item.Visibility = Visibility.Visible;
-            }
+            schedules = new Schedules[] { new Schedules(Lbl0), new Schedules(Lbl1), new Schedules(Lbl2) };
             XElement x = XElement.Load(AppDomain.CurrentDomain.BaseDirectory + "/File/Schedule.xml");
             int i = 0;
             foreach (var field in x.Elements())
@@ -45,18 +50,58 @@ namespace TimeMix
                 {
                     break;
                 }
-               ((Label)Controls[i]).Content = field.Attribute("Name").Value;
-                DateTime d = Convert.ToDateTime(field.Attribute("Date").Value);
-                ((Label)Controls[i]).Content += "剩余" + ((d - DateTime.Now).Days) + "天";
+                schedules[i].Name = field.Attribute("Name").Value;
+                schedules[i].Time = Convert.ToDateTime(field.Attribute("Date").Value);
                 i++;
             }
-            foreach (var item in Controls)
+
+        }
+        /// <summary>
+        /// 更新数据
+        /// </summary>
+        public void Update()
+        {
+            foreach (var item in schedules)
             {
-                if ((string)((Label)item).Content == "")
+                item.Update();
+            }
+        }
+
+        class Schedules
+        {
+            string name;
+            DateTime time;
+            Label label;
+
+            public Schedules(Label label)
+            {
+                this.label = label;
+            }
+
+            public string Name { get => name; set => name = value; }
+            public DateTime Time { get => time; set => time = value; }
+            public Label Label { get => label; set => label = value; }
+
+            public override string ToString()
+            {
+                string s = Name + "剩余" + ((Time - DateTime.Now).Days) + "天";
+                return s;
+            }
+            public void Update()
+            {
+                if (name == null)
                 {
-                    item.Visibility = Visibility.Collapsed;
+
+                    label.Visibility = Visibility.Collapsed;
                 }
+                else
+                {
+                    label.Visibility = Visibility.Visible;
+
+                }
+                label.Content = this.ToString();
             }
         }
     }
+
 }
